0408c58a974c883c9084dda91a401eaf2583d248
Matt Forrest <matt.forrest@dimensionstechnologies.com>
2024-11-29 15:32:35 +0000

    WI#27208

--- app.js
+++ app.js
@@ -19,12 +19,7 @@ define(function(require) {
 		css: [ 'app' ],
 
 		i18n: {
-			'de-DE': { customCss: false },
-			'en-US': { customCss: false },
-			'fr-FR': { customCss: false },
-			'ru-RU': { customCss: false },
-			'es-ES': { customCss: false },
-			'fr-CA': { customCss: false }
+			'en-US': { customCss: false }
 		},
 
 		requests: {},
